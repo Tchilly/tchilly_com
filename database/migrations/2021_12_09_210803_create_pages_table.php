a78771100fd1d8dcc776f5b7e41940aca7c15aca
Kjell-Magnus Vike <magnus@vike.se>
2022-03-18 22:20:19 +0100

    Removes fulltext search

--- database/migrations/2021_12_09_210803_create_pages_table.php
+++ database/migrations/2021_12_09_210803_create_pages_table.php
@@ -17,7 +17,7 @@ class CreatePagesTable extends Migration
             $table->id();
             $table->string('title');
             $table->string('slug')->unique();
-            $table->fullText('body')->nullable();
+            $table->longText('body')->nullable();
             $table->string('template');
             $table->softDeletes();
             $table->timestamps();
