4a87b8512148d49086af9e14ef7af5814094021f
Kjell-Magnus Vike <magnus@vike.se>
2022-03-18 22:39:16 +0100

    Optimizes js libs

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,4 @@
-window._ = require('lodash');
+//window._ = require('lodash');
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -6,9 +6,8 @@ window._ = require('lodash');
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-window.axios = require('axios');
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+// window.axios = require('axios');
+// window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
@@ -28,9 +27,9 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 // });
 
 // Check that service workers are supported
-if ('serviceWorker' in navigator) {
+if ("serviceWorker" in navigator) {
     // Use the window load event to keep the page load performant
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js');
+    window.addEventListener("load", () => {
+        navigator.serviceWorker.register("/service-worker.js");
     });
 }
