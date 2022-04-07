d18ed27da9a0169e7233f67f7d3b75c208d613b1
Kjell-Magnus Vike <magnus@vike.se>
2022-04-07 20:47:06 +0200

    Adds categories to seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -14,8 +14,24 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        Category::factory()
-            ->count(20)
-            ->create();
+
+        $categories = [
+            ['id' => 1, 'title' => 'Audio', 'body' => "<p>Where words fail, music speaks. - Hans Christian Andersen</p>"],
+            ['id' => 2, 'title' => 'Tech', 'body' => "<p>I'm sorry Dave, I'm afraid I can't do that.</p>"],
+            ['id' => 3, 'title' => 'Games', 'body' => "<p>Reality is broken. Game designers can fix it. - Jane McGonigal</p>"],
+            ['id' => 4, 'title' => 'Code', 'body' => "<p>The most damaging phrase in the language is... it's always been done this way. - Grace Hopper</p>"],
+            ['id' => 5, 'title' => 'Life', 'body' => "<p>In order to write about life first you must live it. – Ernest Hemingway</p>"],
+            ['id' => 99, 'title' => 'Uncategorized', 'body' => "<p>Lorem ipsum</p>"],
+        ];
+
+        foreach ($categories as $category) {
+            Category::create($category);
+        }
+
+//        Category::factory()
+//            ->count(20)
+//            ->create();
+
+
     }
 }
