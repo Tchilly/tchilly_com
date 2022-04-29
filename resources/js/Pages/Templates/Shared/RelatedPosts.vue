aab143a44a4249dc44ffcf32f73597dd4e3cfd0e
Kjell-Magnus Vike <magnus@vike.se>
2022-04-29 15:02:36 +0200

    Fixes grid

--- resources/js/Pages/Templates/Shared/RelatedPosts.vue
+++ resources/js/Pages/Templates/Shared/RelatedPosts.vue
@@ -11,9 +11,7 @@ const props = defineProps({
         <div class="relative mx-auto max-w-7xl py-16 px-4 sm:px-6 lg:px-8">
             <h2 class="text-2xl font-bold text-white">Similar posts</h2>
 
-            <div
-                class="mx-auto mt-8 grid max-w-lg gap-8 lg:max-w-none lg:grid-cols-3"
-            >
+            <div class="mx-auto mt-8 grid gap-8 md:grid-cols-2 lg:grid-cols-3">
                 <template v-for="post in posts" :key="post.id">
                     <PostCard :post="post" />
                 </template>
