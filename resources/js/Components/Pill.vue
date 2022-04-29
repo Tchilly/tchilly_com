4ef0e687dcb05712b9ef58235845aa2afe692df5
Kjell-Magnus Vike <magnus@vike.se>
2022-04-29 15:02:20 +0200

    Minor cleanup

--- resources/js/Components/Pill.vue
+++ resources/js/Components/Pill.vue
@@ -13,40 +13,38 @@ defineProps({
 });
 
 const classes =
-    "group inline-flex items-center rounded-full bg-dark-300 px-6 py-1 text-white shadow-lg  sm:text-base lg:text-sm xl:text-base";
+    "group inline-flex items-center rounded-full bg-dark-300 px-6 py-1 text-white shadow-md sm:text-base lg:text-sm xl:text-base";
 </script>
 
 <template>
-    <div>
-        <span v-if="as == 'span'" :class="classes">
-            <slot></slot>
+    <span v-if="as == 'span'" :class="classes">
+        <slot></slot>
 
-            <span class="text-sm">{{ title }}</span>
+        <span class="text-sm">{{ title }}</span>
 
-            <ArrowNarrowRightIcon
-                v-if="chevron"
-                aria-hidden="true"
-                class="ml-2 -mr-2 h-5 w-5 text-gray-500"
-            />
-        </span>
+        <ArrowNarrowRightIcon
+            v-if="chevron"
+            aria-hidden="true"
+            class="ml-2 -mr-2 h-5 w-5 text-gray-500"
+        />
+    </span>
 
-        <a
-            v-else
-            :class="classes"
-            :href="href"
-            :target="target"
-            class="hover:bg-dark-200 hover:text-gray-200 focus:outline-none focus:ring-2 focus:ring-primary-300 focus:ring-offset-2 focus:ring-offset-gray-900"
-            rel="noreferrer"
-        >
-            <slot></slot>
+    <a
+        v-else
+        :class="classes"
+        :href="href"
+        :target="target"
+        class="hover:bg-dark-200 hover:text-gray-200 focus:outline-none focus:ring-2 focus:ring-primary-300 focus:ring-offset-2 focus:ring-offset-gray-900"
+        rel="noreferrer"
+    >
+        <slot></slot>
 
-            <span class="text-sm">{{ title }}</span>
+        <span class="text-sm">{{ title }}</span>
 
-            <ArrowNarrowRightIcon
-                v-if="chevron"
-                aria-hidden="true"
-                class="ml-2 -mr-2 h-5 w-5 text-gray-500"
-            />
-        </a>
-    </div>
+        <ArrowNarrowRightIcon
+            v-if="chevron"
+            aria-hidden="true"
+            class="ml-2 -mr-2 h-5 w-5 text-gray-500"
+        />
+    </a>
 </template>
