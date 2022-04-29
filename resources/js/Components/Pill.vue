4eac116a6866cbc480f06752433d654e8d034010
Kjell-Magnus Vike <magnus@vike.se>
2022-04-30 00:25:21 +0200

    Adds option for size and color

--- resources/js/Components/Pill.vue
+++ resources/js/Components/Pill.vue
@@ -6,6 +6,8 @@ defineProps({
     title: String,
     as: String,
     chevron: { type: Boolean, default: false },
+    small: { type: Boolean, default: false },
+    light: { type: Boolean, default: false },
     target: {
         type: String,
         default: "_self",
@@ -13,19 +15,28 @@ defineProps({
 });
 
 const classes =
-    "group inline-flex items-center rounded-full bg-dark-300 px-6 py-1 text-white shadow-md sm:text-base lg:text-sm xl:text-base";
+    "group inline-flex items-center rounded-full bg-dark-300 px-6 py-1 text-white shadow-md text-base";
 </script>
 
 <template>
-    <span v-if="as == 'span'" :class="classes">
+    <span
+        v-if="as == 'span'"
+        :class="[
+            'group inline-flex items-center rounded-full bg-dark-300 px-6 py-1 text-base text-white shadow-md',
+            small && 'px-[.75rem] py-0.5',
+            light && '!bg-gray-200 shadow-none',
+        ]"
+    >
         <slot></slot>
 
-        <span class="text-sm">{{ title }}</span>
+        <span :class="['text-sm', small && 'text-xs', light && 'text-dark']">{{
+            title
+        }}</span>
 
         <ArrowNarrowRightIcon
             v-if="chevron"
+            :class="['ml-2 -mr-2 h-5 w-5 text-gray-500', small && 'h-4 w-4']"
             aria-hidden="true"
-            class="ml-2 -mr-2 h-5 w-5 text-gray-500"
         />
     </span>
 
